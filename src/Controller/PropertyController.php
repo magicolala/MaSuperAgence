b3dfe4aafe5426cc84c19d29b469fa478ef96a2d
magicolala <cedric.oloa@laposte.net>
2019-02-15 02:02:52 +0100

    doctrine

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/biens", name="property.index")
      * @return Response
@@ -16,6 +28,25 @@ class PropertyController extends AbstractController
     {
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     * @return Response
+     */
+    public function show(Property $property, string $slug): Response
+    {
+        if ($property->getSlug() !== $slug) {
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+
+        return $this->render('property/show.html.twig', [
+            'current_menu' => 'properties',
+            'property' => $property
         ]);
     }
 }
